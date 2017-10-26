820474d8b5c93a3a60a558bc0b562c3785d34e5c
Gabriel Bur <bielbur@gmail.com>
2017-10-26 12:14:47 -0200

    Melhorando ui

--- app/src/main/java/Classes/CustomExpandableListView.java
+++ app/src/main/java/Classes/CustomExpandableListView.java
@@ -76,7 +76,6 @@ public class CustomExpandableListView extends BaseExpandableListAdapter {
                     convertView = inflater.inflate(R.layout.list_group,null);
                 }
         TextView lblListHeader = (TextView)convertView.findViewById(R.id.lblListHeader);
-        lblListHeader.setTypeface(null, Typeface.BOLD);
         lblListHeader.setText(headerTitle);
         return convertView;
     }
